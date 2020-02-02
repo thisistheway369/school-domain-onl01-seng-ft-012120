5c575bc95ef8c07bb5b0f604a8ca3aca9b10f744
thisistheway369 <thisistheway369@icloud.com>
2020-02-02 04:39:03 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+	attr_accessor :roster, :name, :grade 
+	attr_reader :school 
+	
+	def initialize(school)
+    school = school
+  end
+  
+  def roster
+  	roster = {}
+  end 
+  
+  def add_student(name,grade)
+  	roster[grade] = []
+  	roster[grade] << name
+  		
+  end
+
+end
+
+
+School.new("R").roster.add_student("john",9)
